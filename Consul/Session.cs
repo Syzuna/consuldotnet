372c31141dd2a56dc3ea79cefe6c1d7be6679a1e
Syzuna <syzuna@t-teske.de>
2020-11-23 00:07:35 +0100

    removed old SessionBehaviorConverter

--- Consul/Session.cs
+++ Consul/Session.cs
@@ -62,34 +62,6 @@ namespace Consul
         }
     }
 
-    //public class SessionBehaviorConverter : JsonConverter
-    //{
-    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    //    {
-    //        serializer.Serialize(writer, ((SessionBehavior)value).Behavior);
-    //    }
-
-    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
-    //        JsonSerializer serializer)
-    //    {
-    //        var behavior = (string)serializer.Deserialize(reader, typeof(string));
-    //        switch (behavior)
-    //        {
-    //            case "release":
-    //                return SessionBehavior.Release;
-    //            case "delete":
-    //                return SessionBehavior.Delete;
-    //            default:
-    //                throw new ArgumentException("Unknown session behavior value during deserialization");
-    //        }
-    //    }
-
-    //    public override bool CanConvert(Type objectType)
-    //    {
-    //        return objectType == typeof(SessionBehavior);
-    //    }
-    //}
-
     public class SessionBehaviorConverter : JsonConverter<SessionBehavior>
     {
         public override SessionBehavior Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
